bc0ef4dc926873e380b712de34badb0bb5dc989e
Studly <joel@webrelics.xyz>
2026-06-27 00:39:19 +0200

    feat: add doc loader

---
+++ loader.py
@@ -0,0 +1,16 @@
+import os
+
+def load_docs(folder="docs"):
+    docs = {}
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+        print(f"[loader] created '{folder}/'; place .txt files there.")
+        return docs
+    for fname in sorted(os.listdir(folder)):
+        if fname.endswith(".txt"):
+            path = os.path.join(folder, fname)
+            with open(path, "r", encoding="utf-8", errors="ignore") as f:
+                docs[fname] = f.read()
+    if not docs:
+        print(f"[loader] no .txt files found in '{folder}/'.")
+    return docs
